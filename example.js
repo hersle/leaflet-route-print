98282dbeab6f401ccc3bf835fa39bc7dcad9928e
Herman Sletmoen <hermansletmoen@gmail.com>
2021-08-10 19:12:41 +0200

    Added leaflet-routing library with straight-line routing

--- example.js
+++ example.js
@@ -8,3 +8,30 @@ var tl = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 });
 tl.addTo(map);
 map.setView(center, 15);
+
+var routing = new L.Routing({
+	position: 'topright',
+	routing: {
+		router: function(p1, p2, cb) { 
+			// straight line
+			cb(null, L.polyline([p1, p2]));
+		}
+	}, 
+	tooltips: {
+		waypoint: 'Waypoint. Drag to move; Click to remove.',
+		segment: 'Drag to create a new waypoint'
+	}, 
+	styles: {         // see http://leafletjs.com/reference.html#polyline-options
+		trailer: {},  // drawing line
+		track: {},    // calculated route result
+		nodata: {},   // line when no result (error)
+	}, 
+	shortcut: {
+		draw: {
+			enable: 68,   // 'd'
+			disable: 81   // 'q'
+		}
+	}
+});
+map.addControl(routing);
+routing.draw(true);
