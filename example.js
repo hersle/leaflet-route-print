99e19fc71f465ae5cf00e284615a6666d881b223
Herman Sletmoen <hermansletmoen@gmail.com>
2021-08-10 19:25:51 +0200

    Added print control button to map

--- example.js
+++ example.js
@@ -9,6 +9,38 @@ var tl = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 tl.addTo(map);
 map.setView(center, 15);
 
+function addControlButton(pos, text, title, cb) {
+	L.Control.CustomButton = L.Control.extend({
+		onAdd: function(map) {
+			var div = document.createElement("div");
+			div.className += " leaflet-bar";
+			var el = document.createElement("a");
+			el.innerHTML = text;
+			el.className += " leaflet-control-zoom-in";
+			el.style += " vertical-align: middle;";
+			el.href = "#";
+			el.title = title;
+			el.setAttribute("aria-label", title);
+			el.role = "Button";
+			div.appendChild(el);
+			L.DomEvent.on(el, "click", function(event) {
+				L.DomEvent.stopPropagation(event); // prevent from reaching underlying map
+				cb(event);
+			});
+			return div;
+		},
+		onRemove: function(map) {
+		},
+	});
+
+	var button = new L.Control.CustomButton({position: pos});
+	button.addTo(map);
+}
+
+addControlButton("topleft", "P", "Print", function(event) {
+	console.log(event);
+});
+
 var routing = new L.Routing({
 	position: 'topright',
 	routing: {
