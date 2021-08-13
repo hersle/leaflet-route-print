f37ac7261942ca3bb13d209ac7ec475dc83ccc44
Herman Sletmoen <hermansletmoen@gmail.com>
2021-08-13 16:20:46 +0200

    Divide route into print-rectangles of a given size

--- example.js
+++ example.js
@@ -48,20 +48,93 @@ function max(a, b) {
 	return a > b ? a : b;
 }
 
+function rectIsOk(r, w, h) {
+	return r[1][0] - r[0][0] <= w && r[1][1] - r[0][1] <= h;
+}
+
+function growRect(r, x, y) {
+	return [[min(r[0][0], x), min(r[0][1], y)], [max(r[1][0], x), max(r[1][1], y)]];
+}
+
+function intersectSegments(l1, l2) {
+	// see https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line_segment
+	const [[x1, y1], [x2, y2]] = l1;
+	const [[x3, y3], [x4, y4]] = l2;
+	var d = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4);
+	var t = ((x1-x3)*(y3-y4) - (y1-y3)*(x3-x4)) / d;
+	var u = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / d;
+	if (d != 0 && t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+		var x = x1 + t*(x2-x1);
+		var y = y1 + t*(y2-y1);
+		return [x, y];
+	} else {
+		return undefined
+	}
+}
+
+function intersectRectangleSegment(r, s) {
+	var p1 = r[0];
+	var p3 = r[1];
+	var p2 = [p3[0], p1[1]];
+	var p4 = [p1[0], p3[1]];
+	var s1 = [p1, p2];
+	var s2 = [p2, p3];
+	var s3 = [p3, p4];
+	var s4 = [p4, p1];
+	var ss = [s1, s2, s3, s4];
+	for (var side of ss) {
+		var p = intersectSegments(s, side);
+		// don't register intersection if it is in the beginning corner
+		if (p != undefined && p[0] != s[0][0] && p[1] != s[0][1]) {
+			return p; // intersect with a side
+		}
+	}
+	return undefined; // no intersection
+}
+
+function growRectBounded(r, d, w, h) {
+	var min = r[0];
+	var max = r[1];
+	var size = [max[0] - min[0], max[1] - min[1]];
+
+	if (d[0] > 0) {
+		max[0] = max[0] + w - size[0];
+	} else {
+		min[0] = min[0] - w + size[0];
+	}
+
+	if (d[1] > 0) {
+		max[1] = max[1] + h - size[1];
+	} else {
+		min[1] = min[1] - h + size[1];
+	}
+
+	return [min, max];
+}
+
 function coverLineWithRectangles(l, w, h) {
-	var xmin = l[0][0];
-	var ymin = l[0][1];
-	var xmax = l[0][0];
-	var ymax = l[0][1];
-	for (var i = 1; i < l.length; i++) {
+	var rects = [];
+	var intersections = [];
+	var rect = [[l[0][0], l[0][1]], [l[0][0], l[0][1]]];
+	for (var i = 0; i < l.length; i++) {
 		var x = l[i][0];
 		var y = l[i][1];
-		xmin = min(xmin, x);
-		ymin = min(ymin, y);
-		xmax = max(xmax, x);
-		ymax = max(ymax, y);
+		var grect = growRect(rect, x, y);
+		if (i == 0 || rectIsOk(grect, w, h)) {
+			rect = grect;
+		} else {
+			var s = [l[i-1], l[i]];
+			var vs = [s[1][0]-s[0][0],s[1][1]-s[0][1]];
+			rect = growRectBounded(rect, vs, w, h);
+			rects.push(rect);
+			var p = intersectRectangleSegment(rect, s);
+			intersections.push(p);
+			l.splice(i, 0, p);
+			var rect = [[l[i][0], l[i][1]], [l[i][0], l[i][1]]];
+		}
 	}
-	return [[xmin, ymin], [xmax, ymax]];
+	rects.push(rect);
+	return [rects, intersections];
 }
 
 addControlButton("topleft", "P", "Print", function(event) {
@@ -69,11 +142,15 @@ addControlButton("topleft", "P", "Print", function(event) {
 	for (var i = 0; i < points.length; i++) {
 		points[i] = [points[i].lat, points[i].lng];
 	}
-	var rect = coverLineWithRectangles(points, 0, 0);
+	const [rects, intersections] = coverLineWithRectangles(points, 0.01, 0.01);
 
 	rectGroup.clearLayers();
-	var rect = L.rectangle(rect);
-	rectGroup.addLayer(rect);
+	for (const rect of rects) {
+		rectGroup.addLayer(L.rectangle(rect));
+	}
+	for (const p of intersections) {
+		rectGroup.addLayer(L.circleMarker(p, {color: "red"}));
+	}
 });
 
 var routing = new L.Routing({
